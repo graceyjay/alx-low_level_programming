a7c46852a6c060f6fba46d848040c378131f3b8b
root <gracejibaniya95@gmail.com>
2022-12-12 17:24:32 -0800

    my quote again

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-
-
 /**
  * main - prints exactly and that piece of art is useful
- * 
- * Return: Always 0 (Success)
+ *
+ * Return: 0
  */
 
 int main(void)
