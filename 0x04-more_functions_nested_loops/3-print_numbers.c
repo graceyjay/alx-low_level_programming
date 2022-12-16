e43751a57a967e1ff1c9f87ffc1424eb315aa15e
root <gracejibaniya95@gmail.com>
2022-12-15 19:36:51 -0800

    numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * prints_numbers - checks for a digit (0 through 9)
+ *
+ * Return: 0
+ */
+
+viod print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c < 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n')
+}
