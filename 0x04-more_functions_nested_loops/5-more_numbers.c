0f4dbb15ecc1b85d2bb34411eca0e46002218084
root <gracejibaniya95@gmail.com>
2022-12-15 20:04:10 -0800

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,14 +12,14 @@ void more_numbers(void)
 
 	for (number1 = 0; number1 < 10; number1++)
 	{
-		for (number2 = 0; number2 <= 14; Number2++)
+		for (number2 = 0; number2 <= 15; Number2++)
 		{
-			if (number2 > 9)
+			if (number2 > 10)
 			{
 				_putchar((number2 / 10) + '0');
 			}
 			_putchar((number2 % 10) + '0');
 		}
-		_putchar(10);
+		_putchar('\n');
 	}
 }
